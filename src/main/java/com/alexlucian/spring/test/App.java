103e64714afcfb672cfeb3c31c66138a6b60caac
Francu Alexandru Lucian <alexandru.lucian89@gmail.com>
2015-05-25 22:06:02 +0100

    Spring sample

---
+++ src/main/java/com/alexlucian/spring/test/App.java
@@ -0,0 +1,25 @@
+package com.alexlucian.spring.test;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App {
+
+	public static void main(String[] args) {
+		
+		ApplicationContext context = new ClassPathXmlApplicationContext("com/alexlucian/spring/test/beans/beans.xml");
+		
+		Person person = (Person)context.getBean("person");
+		person.speak();
+		
+				
+		Address address = (Address)context.getBean("address");
+		System.out.println(address);
+		
+		System.out.println(person);
+		
+		((ClassPathXmlApplicationContext)context).close();
+
+	}
+
+}
